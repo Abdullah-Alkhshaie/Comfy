76871508fe2a1f19efc6057e110b20ff3e2c18fb
abdullah-alkhshaie <abdullahalkhshaie@gmail.com>
2023-12-09 15:56:07 +0300

    add: initial files

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import ProductProvider from "./context/ProductContext.jsx";
+import "./index.css";
+import CartProvider from "./context/CartContext.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <CartProvider>
+    <ProductProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ProductProvider>
+  </CartProvider>
+);
